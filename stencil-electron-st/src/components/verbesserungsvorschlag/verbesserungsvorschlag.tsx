17fd13585febf0b28520e8edd965e328365b670e
alicia-klln <alicia.killian@hotmail.de>
2020-04-23 09:42:18 +0200

    verbesserungsvorschlag-dateien eingefügt

---
+++ stencil-electron-st/src/components/verbesserungsvorschlag/verbesserungsvorschlag.tsx
@@ -0,0 +1,28 @@
+import { Component, Prop,  } from '@stencil/core';
+
+@Component({
+  tag: 'verbesserungs-vorschlag',
+  styleUrl: 'verbesserungsvorschlag.css',
+  shadow: true,
+})
+export class VerbesserungsVorschlag {
+  @Prop() test: string;
+  
+  handleClick(event: UIEvent) {
+    
+    // var inhalt = document.getElementById('save-button').value;
+    alert("Du hast auf Speichern gedrückt");
+    
+  }
+
+  render() {
+    return (
+        <div class="verbesserungs-vorschlag">
+          <h1 class="ueberschrift2">Verbesserungsvorschläge</h1>
+          <textarea id="input"></textarea>
+          <button id="save-button" onClick={ (event: UIEvent) => this.handleClick(event)}>Speichern</button>
+        </div>
+    );
+  }
+  
+}
