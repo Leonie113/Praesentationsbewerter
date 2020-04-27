ca220c8d869c6e1582bf7cd3b6fc9697dd2c685c
Nicolehoefler19 <57350812+Nicolehoefler19@users.noreply.github.com>
2020-04-27 09:18:55 +0200

    Added AB und Bewertungsseite

--- stencil-electron-st/src/components/app-bewertung/app-bewertung.tsx
+++ stencil-electron-st/src/components/app-bewertung/app-bewertung.tsx
@@ -1,32 +1,53 @@
-import { Component } from '@stencil/core';
+import { Component, h } from '@stencil/core';
+//import { AufklappButton } from '../aufklapp-button/aufklapp-button';
 
 @Component({
   tag: 'app-bewertung',
   styleUrl: 'app-bewertung.css',
   shadow: true
 })
+
 export class AppHome {
 
   render() {
     return (
-      <div class='app-home'>
-        <p>
-          Bewertung
-        </p>
-        <aufklapp-button></aufklapp-button>
-          <regler-wertung></regler-wertung>
-          <export-button></export-button>
-          <anmerkungs-feld></anmerkungs-feld>
-        <stencil-route-link url='/profile/stencil'>
-          <button>
-            zurück
-          </button>
-        </stencil-route-link>
-        <stencil-route-link url='/profile/stencil'>
-          <button>
-            weiter
-          </button>
-        </stencil-route-link>
+      <div class='app-bewertung'>
+        <header-leonie class="header-leonie"></header-leonie>
+        <h2 class='title'>
+          Bewertung - Projektvortrag
+        </h2>
+        <aufklapp-button class="aufklappbuttons"
+          Ueberschrift="Thematische Ausarbeitung" 
+          Kriterium1="Fachliche Bearbeitung (unter Berücksichtung des Schwierigkeitsgrads)"
+          Kriterium2="Nutzung von Fachwissen"
+          Kriterium3="Struktur"
+          Kriterium4="Dokumentation"
+        ></aufklapp-button>
+
+        <aufklapp-button-kurz class="aufklappbuttons"
+        Ueberschrift="Produkt"
+        Kriterium1="Umsetzbarkeit des Ergebnisses"
+        Kriterium2="Kreativität"
+        >
+
+        </aufklapp-button-kurz>
+
+        <aufklapp-button class="aufklappbuttons" 
+          Ueberschrift="Herangehensweise"
+          Kriterium1="Methodik"
+          Kriterium2="Selbstständigkeit, Eigeninitiative"
+          Kriterium3="Kommunikation"
+          Kriterium4="Dokumentation"
+        ></aufklapp-button>
+
+        <aufklapp-button class="aufklappbuttons-last" 
+          Ueberschrift="Präsentation"
+          Kriterium1="Rhetorik"
+          Kriterium2="Ansprache"
+          Kriterium3="Medien"
+          Kriterium4="Diskussion"
+        ></aufklapp-button>
+        <footer-leonie class="footer-leonie"></footer-leonie>
       </div>
     );
   }
