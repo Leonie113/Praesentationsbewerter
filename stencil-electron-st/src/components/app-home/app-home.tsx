292842ee60840f5c912804e1ab6e3ee82cb4eac5
Leonie113 <57359744+Leonie113@users.noreply.github.com>
2020-05-05 10:16:18 +0200

    Start + Ihr Name

--- stencil-electron-st/src/components/app-home/app-home.tsx
+++ stencil-electron-st/src/components/app-home/app-home.tsx
@@ -12,10 +12,13 @@ export class AppHome {
       <div class='app-home'>
         <headline-mittig text="Herzlich Willkommen beim Präsentationsbewerter"></headline-mittig>
         <img id="presentation" src="/assets/presentation.svg"></img>
-        <div >
+        <div class="weiter">
           <stencil-route-link url="/profile/stencil">
           <weiter-button></weiter-button>
           </stencil-route-link>
+          <div class="weiter-text">
+            <h1>Start</h1>
+          </div>
         </div>
       </div>
     );
