3ad5b55afddb43398f42088ffa73deb54f264b5c
Leonie113 <57359744+Leonie113@users.noreply.github.com>
2020-05-13 09:40:01 +0200

    veränderungen

--- stencil-electron-st/src/components/header-leonie/header-leonie.tsx
+++ stencil-electron-st/src/components/header-leonie/header-leonie.tsx
@@ -9,7 +9,7 @@ export class HeaderLeonie {
     render() {
         return <div class="header">
             <stencil-route-link url="/">
-           
+                <div id="home">Home</div>
             </stencil-route-link>
 
             </div>
