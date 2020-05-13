4481f49e6b5040de1a9b21c4c1538d3d39492a5d
Leonie113 <57359744+Leonie113@users.noreply.github.com>
2020-05-13 10:16:21 +0200

    icon

--- stencil-electron-st/main.js
+++ stencil-electron-st/main.js
@@ -8,7 +8,7 @@ let win;
 
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({ width: 1920, height: 1080, icon: path.join(__dirname, 'build/icons/icon.icns') });
+  win = new BrowserWindow({ width: 1920, height: 1080, icon: path.join(__dirname, 'assets/icons/png/presentation.png') });
   // and load the index.html of the app.
   win.loadURL(
     isDev
